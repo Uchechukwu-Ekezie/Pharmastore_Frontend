656f2faf88e67b2a1ad48f2365a8ca1935c66e42
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-08-13 21:09:15 +0100

    Fix case sensitivity issue for Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,29 +1,40 @@
-import React from 'react'
-import CategoryList from '../Component/CategoryList'
-import BannerProduct from '../Component/BannerProduct'
-import HorizontalCardProduct from '../Component/HorizontalCardProduct'
-import VerticalCardProduct from '../Component/VerticalCardProduct'
+import React from "react";
+import CategoryList from "../Component/CategoryList";
+import BannerProduct from "../Component/BannerProduct";
+import HorizontalCardProduct from "../Component/HorizontalCardProduct";
+import VerticalCardProduct from "../Component/VerticalCardProduct";
 
 function Home() {
   return (
     <div>
-      <CategoryList/>
-      <BannerProduct/>
+      <CategoryList />
+      <BannerProduct />
 
-      <HorizontalCardProduct category={"firstaid"} heading={"Top's First Aids"}/>
-      <HorizontalCardProduct category={"watches"} heading={"Popular's Watches"}/>
+      <HorizontalCardProduct
+        category={"firstaid"}
+        heading={"Top's First Aids"}
+      />
+      <HorizontalCardProduct
+        category={"watches"}
+        heading={"Popular's Watches"}
+      />
 
-
-      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"}/>
-      <VerticalCardProduct category={"Mouse"} heading={"Mouse"}/>
-      <VerticalCardProduct category={"televisions"} heading={"Televisions"}/>
-      <VerticalCardProduct category={"camera"} heading={"Camera & Photography"}/>
-      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"}/>
-      <VerticalCardProduct category={"speakers"} heading={"Bluetooth Speakers"}/>
-      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"}/>
-      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"}/>
+      <VerticalCardProduct category={"mobiles"} heading={"Mobiles"} />
+      <VerticalCardProduct category={"Mouse"} heading={"Mouse"} />
+      <VerticalCardProduct category={"televisions"} heading={"Televisions"} />
+      <VerticalCardProduct
+        category={"camera"}
+        heading={"Camera & Photography"}
+      />
+      <VerticalCardProduct category={"earphones"} heading={"Wired Earphones"} />
+      <VerticalCardProduct
+        category={"speakers"}
+        heading={"Bluetooth Speakers"}
+      />
+      <VerticalCardProduct category={"refrigerator"} heading={"Refrigerator"} />
+      <VerticalCardProduct category={"trimmers"} heading={"Trimmers"} />
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
